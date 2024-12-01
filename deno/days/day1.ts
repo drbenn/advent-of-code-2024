b7991d0cf37a7814e80726069fb9985d4b38ed88
drbenn <bennett.daniel@gmail.com>
2024-12-01 09:40:42 -0500

    day 1 part 2 complete

--- deno/days/day1.ts
+++ deno/days/day1.ts
@@ -21,6 +21,7 @@ export function part1(): number {
     listOne.push(parsedData[i][0]);
     listTwo.push(parsedData[i][1]);
   }
+
   
   // console.log(listOne);
   
@@ -46,4 +47,47 @@ export function part1(): number {
   // console.log(sumOfDifferences);
   
   return sumOfDifferences;
+}
+
+export function part2(): number {
+
+  // CREATE LISTS AGAIN FROM PROBLEM DATA
+    // create lists from raw data
+    const listOne: number[] = [];
+    const listTwo: number[] = [];
+
+    // console.log(puzzleInput);
+    const rows = puzzleInput.trim().split('\n');
+    const parsedData = rows.map(row => {
+      const [col1, col2] = row.trim().split(/\s+/).map(Number);
+      return [ col1, col2 ]
+    })
+    // console.log(parsedData);
+    for (let i = 0; i < parsedData.length; i++ ) {
+      listOne.push(parsedData[i][0]);
+      listTwo.push(parsedData[i][1]);
+    }
+
+  // find out how often each number from left list  appears in the right list
+  const listTwoAsCount: number[] = [];
+
+  for (let i = 0; i < listOne.length; i++) {
+    // find list one value in list two as array
+    const listOneValueInListTwo: number[] = listTwo.filter((list2Item: number) => list2Item === listOne[i]);
+    // console.log(listOneValueInListTwo.length);
+    // use array length as count of occurence and push to new list
+    listTwoAsCount.push(listOneValueInListTwo.length);
+  }
+
+
+  // multiply left list value by its count of occurence in the right list
+  // calculate total similarity score by adding upo each number in the left list
+  let similarityScore: number = 0;
+
+  for (let i = 0; i < listOne.length; i++) {
+    similarityScore += listOne[i] * listTwoAsCount[i];
+  }
+  console.log('similarity score: ', similarityScore);
+  
+  return similarityScore;
 }
--- deno/main.ts
+++ deno/main.ts
@@ -1,9 +1,10 @@
-import { part1 } from './days/day1.ts';
+import * as day1 from './days/day1.ts';
 import * as day2 from './days/day2.ts';
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log(part1());
+  console.log(day1.part1());
+  console.log(day1.part2());
   // console.log(day2.run());
   // console.log("\x1b[37;44m This is white text on a blue background! \x1b[0m");
   // console.log("\x1b[37;42m This is black text on a green background! \x1b[0m");
